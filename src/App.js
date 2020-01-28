835a8650c069b95ea0e775b78ab82ef828ea4de7
Yo shinzato <s19008@std.it-college.ac.jp>
2020-01-28 15:59:52 +0900

    sumple

--- src/App.js
+++ src/App.js
@@ -1,39 +1,5 @@
 import React from 'react'
 
-class App extends React.Component {
-    constructor (props) {
-      super(props)
-    
-      this.state = {
-        name: 'クジラ',
-        age: 22,
-      
-        hobby: '読書'
-      }
-    }
-
-    doChange (e) {
-      const userValue = e.target.value
-      const key = e.target.name
-      this.setState({[key]: userValue})
-    }
-
-    doSubmit (e) {
-      e.preventDefault()
-      const j = JSON.stringify(this.state)
-      window.alert(j)
-    }
-    render () {
-      const doSubmit = (e) => this.doSubmit(e)
-      const doChange = (e) => this.doChange(e)
-      return (
-        <form onSubmit={doSubmit}>
-            <div><label>
-                名前: <br />
-                <input name='name'
-                    type='text'
-                    )}
-}
 
 
 
